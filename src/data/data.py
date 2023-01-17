fa9bf541c287cf793f88f07be88d2f7c1e8c314f
Helen-Z-storage <helenamanbar21534@gmail.com>
2023-01-16 22:25:41 -0500

    data cleaning finished
    adding lat and lon on every city

--- src/data/data.py
+++ src/data/data.py
@@ -1,52 +1,45 @@
 
 import json
-import requests
+import urllib.request
 
 API_key = "87391d7ad955bd47f528c419e1b7519d"
 
 # Opening JSON file
-with open('country-list.json', 'rb') as openfile:
+with open('capital1.json', 'rb') as openfile:
  
     # Reading from json file
-    country_lst = json.load(openfile)
+    capitals = json.load(openfile)["capitals"]
  
-
-# Opening JSON file
-with open('slim-2.json', 'rb') as openfile1:
- 
-    # Reading from json file
-    city_lst = json.load(openfile1)
 
 lst = []
-n = len(country_lst)
-m = len(city_lst)
-
+n = len(capitals)
 for i in range(n):
-    #print(country_lst[i])
-    curr_country = country_lst[i]['country']
-    curr_cap = country_lst[i]['capital']
-    city_dict = {}
-    for j in range(m):
-        curr_city = city_lst[j]
-        if curr_country == curr_city['name']:
-            city_dict = {
-                "country": curr_country,
-                "city": curr_cap, 
-                "city_id": curr_city['country-code'],
-                "country_code": curr_city['alpha-2']
+    city_data = {}
+    capital = capitals[i]
+
+    url = "http://api.openweathermap.org/geo/1.0/direct?q=" + capital["city"] + ",," + capital["country_code"] + "&appid=" + API_key
+    try:
+        response_API = urllib.request.urlopen(url)
+    except:
+        lst.append(capital)
+        continue
+
+    with response_API:
+        data = response_API.read()
+        # I'm guessing this would output the html source code ?
+        json_data = json.loads(data)
+        if json_data:
+            city_data = {
+                    "country": capital["country"],
+                    "city": capital["city"],
+                    "country_code": capital["country_code"],
+                    "lat": json_data[0]["lat"],
+                    "lon": json_data[0]["lon"]
                 }
-    if city_dict == {}:
-        city_dict = country_lst[i]
-    lst.append(city_dict)
-        
+        else:
+            city_data = capital
+    lst.append(city_data)
+
 
 with open("capital.json", "w") as outfile:
     json.dump(lst, outfile)
-
-city_name = 
-country_code = 
-url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city_name + ",," + country_code + "&appid=" + API_key
-response_API = requests.get(url)
-data = response_API.text
-json_data = json.loads(data)
-print(json_data)
